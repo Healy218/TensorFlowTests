291550e6351b838fe565b993f057e65f64323ba8
Matt Healy <42188555+Healy218@users.noreply.github.com>
2019-04-11 12:49:38 -0500

    added imports
    ready to start testing

--- testing.py
+++ testing.py
@@ -1,1 +1,14 @@
+# Import TensorFlow and TensorFlow Datasets
+import tensorflow as tf
+import tensorflow_datasets as tfds
+tf.logging.set_verbosity(tf.logging.ERROR)
 
+# Helper libraries
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Improve progress bar display
+import tqdm
+import tqdm.auto
+tqdm.tqdm = tqdm.auto.tqdm
